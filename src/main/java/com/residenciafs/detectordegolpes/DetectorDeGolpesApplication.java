3002545a911b4a2955d24aedb40e2caf4dc2e4f0
gzcaue <cauegeraldizanardi@gmail.com>
2026-07-21 16:04:59 -0300

    feat: pastas iniciais de um projeto SpringBoot

---
+++ src/main/java/com/residenciafs/detectordegolpes/DetectorDeGolpesApplication.java
@@ -0,0 +1,13 @@
+package com.residenciafs.detectordegolpes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorDeGolpesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetectorDeGolpesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/residenciafs/detectordegolpes/DetectorDeGolpesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.residenciafs.detectordegolpes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectorDeGolpesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
