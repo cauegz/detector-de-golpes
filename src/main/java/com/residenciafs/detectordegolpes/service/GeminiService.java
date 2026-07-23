367dad66bf3aeb06f20f8212cf6aabe6a4062cc4
gzcaue <cauegeraldizanardi@gmail.com>
2026-07-23 10:47:06 -0300

    refact: mudando para o Spring AI

--- src/main/java/com/residenciafs/detectordegolpes/service/GeminiService.java
+++ src/main/java/com/residenciafs/detectordegolpes/service/GeminiService.java
@@ -2,46 +2,42 @@ package com.residenciafs.detectordegolpes.service;
 
 import com.residenciafs.detectordegolpes.dto.GeminiRequest;
 import com.residenciafs.detectordegolpes.dto.GeminiResponse;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class GeminiService {
-    private final RestClient restClient;
-    private final String apiUrl;
-    private final String apiKey;
+    private final ChatClient chatClient;
+    private final Resource resource;
 
 
-    public GeminiService(@Value("${gemini.api.url}") String apiUrl,
-                         @Value("${gemini.api.key}") String apiKey){
-        this.apiUrl = apiUrl;
-        this.apiKey = apiKey;
-        this.restClient = RestClient.create(); //´ra instanciar o cliente http do spring
+    public GeminiService(ChatClient.Builder chatClientBuilder,
+                         @Value("classpath:prompts/base.txt")
+                         Resource promptResource){
+        this.chatClient = chatClientBuilder.build();
+        this.resource = promptResource;
     }
 
     public String analisarMensagemBase(String mensagem){
-        var body = new GeminiRequest(List.of(
-                new GeminiRequest.Content(List.of(
-                    new GeminiRequest.Part(mensagem)
-                ))
-        ));
+        PromptTemplate promptTemplate =
+                new PromptTemplate(resource);
 
-        try{
-            GeminiResponse resposta = restClient.post()
-                    .uri(apiUrl + "?key="+ apiKey)
-                    .header("Content-Type", "application/json")
-                    .body(body)
-                    .retrieve()
-                    .body(GeminiResponse.class); //json -> record
+        String promptCompleto = promptTemplate.render(
+                Map.of("mensagem", mensagem)
+        );
 
-            //retorna e extrai a resposta
-            return resposta != null ? resposta.extrairTexto() : "Erro, calhou de não receber, o gemini não gosta de vc";
-        }catch(Exception e){
-            System.err.println(e.getMessage());
-            return "Erro";
+        return chatClient
+                .prompt()
+                .user(promptCompleto)
+                .call()
+                .content();
         }
     }
-}
+
