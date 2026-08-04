20d45afc7592af5015fd6211db4eb90382c63c44
Mavinyx <nicollyvianamartins@gmail.com>
2026-08-04 11:19:21 -0300

    feat: adiciona customização de fontes e melhorias na interface do usuário

--- frontend/detector-de-golpes/src/App.jsx
+++ frontend/detector-de-golpes/src/App.jsx
@@ -3,28 +3,26 @@ import { useState } from 'react'
 function App() {
 
   return (
-    <div className="glass min-h-screen flex justify-center items-center flex-col">
-      <div className='bg-primary/20 card-dash rounded-md border p-4 h-125'>
-        <div className='card-title text-primary'>
-         É Golpe?? Descubra!!
-        </div>
+    <div className="bg-gradient-to-r from-base-200 to-error-content text-primary-content min-h-screen flex justify-center items-center flex-col ">
+      <h1 className="text-primary text-2xl md:text-3xl lg:text-4xl font-bold mb-[4%] font-bebasneue tracking-widest">É Golpe?? Descubra!!</h1>
+      <div className='bg-primary-content/20 card-dash rounded-md border p-4 h-125 font-mono'>
         <div className='card-body'>
           <label htmlFor="mensagem" className="label text-base-content">
             Insira a mensagem que deseja analisar:
           </label>
-          <input type="text" className="input" id="mensagem" />
+          <input type="text" className="input w-full" id="mensagem" />
           <label htmlFor="contexto" className="label text-base-content">
             Descreva em que contexto a mesagem foi recebida:
           </label>
-          <input type="text" className="input" id="contexto" />
+          <input type="text" className="input w-full" id="contexto" />
           <label htmlFor="empresa" className="label text-base-content">
            Qual a empresa/remetente responsável pela mensagem:
           </label>
-          <input type="text" className="input" id="empresa" />
+          <input type="text" className="input w-full" id="empresa" />
           <label htmlFor="meioComunicacao" className="label text-base-content">
            Qual o meio de comunicação utilizado:
           </label>
-         <input type="text" placeholder="Ex: empresa@dominio.com" className="input" id="meioComunicacao" />
+         <input type="text" placeholder="Ex: empresa@dominio.com" className="input w-full placeholder:text-white/50" id="meioComunicacao" />
          <button className="btn btn-primary mt-4">Analisar</button>
         </div>
       </div>
