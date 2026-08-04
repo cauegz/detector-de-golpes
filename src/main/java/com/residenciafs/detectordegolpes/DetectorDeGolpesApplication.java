0e558d83fa16f2eb181d1f8c272e6ea603d1a818
Mavinyx <nicollyvianamartins@gmail.com>
2026-08-04 09:17:37 -0300

    feat: implementa a dependencia do springdoc

--- src/main/java/com/residenciafs/detectordegolpes/DetectorDeGolpesApplication.java
+++ src/main/java/com/residenciafs/detectordegolpes/DetectorDeGolpesApplication.java
@@ -9,5 +9,7 @@ public class DetectorDeGolpesApplication {
     public static void main(String[] args) {
         SpringApplication.run(DetectorDeGolpesApplication.class, args);
     }
-
+    //Documentação e testes
+    //http://localhost:8080/swagger-ui/index.html
+    //http://localhost:8080/v3/api-docs
 }
